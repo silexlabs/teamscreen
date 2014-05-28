dcae1a7226236c06eb7353af8b15c00c4fce3021
lexoyo <a.hoyau@silexlabs.org>
2014-05-28 15:04:59 +0200

    refresh as an option in the json data

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 (function($scope) {
     $scope.App = function(){
+        console.log('aaa');
+        $('body').click(function () {
+            $scope.nextPage();
+        });
         this.loadData($scope.Config.dataUrl);
     };
     $scope.App.prototype.loadData = function(url){
@@ -7,13 +11,14 @@
             var urls = data.urls;
             // iframes
             for (idx in urls){
-                var url = urls[idx];
-                $('body').append('<iframe class="page" src="'+url+'" />');
-                console.log('add browser ', name);
+                var url = urls[idx].url;
+                var refresh = urls[idx].refresh;
+                if (typeof(refresh) === 'undefined') refresh = true;
+                $('body').append('<iframe class="page" data-refresh="'+refresh+'" src="'+url+'" />');
             }
             $scope.nextPage();
         }).fail(function(_, message) {
-                console.error( "error", message );
+            console.error( "error", message );
         });
     };
     $scope.nextPage = function(){
@@ -21,8 +26,10 @@
         // refresh previous iframe
 
         var iframe = $('.current-page').first();
-        iframe.attr('src', iframe.attr('src'));
-
+        if (iframe.attr('data-refresh') === 'true') {
+            console.log('refresh');
+            iframe.attr('src', iframe.attr('src'));
+        }
         $('.current-page').removeClass('current-page').next('.page').addClass('current-page');
         if (document.getElementsByClassName('current-page').length === 0){
             console.log('nextPage 1st');
